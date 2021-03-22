01978b6508daf7106240c8c81b3af33f82cbe7a1
flozada <flozada@uao.edu.co>
2021-03-22 01:05:04 -0500

    fixed filters

--- src/pages/roles/index.js
+++ src/pages/roles/index.js
@@ -56,82 +56,27 @@ const Roles = () => {
   const [filterData, setFilterData] = useState({});
   const [showModal, setShowModal] = useState(false);
 
-  const _filterData = (data, filters) => {
-    const { first, last, id, rol, state, phone, email } = filters;
-    let auxDataFirst = data.filter(
-      (user) => user.first.toLowerCase().indexOf(first.toLowerCase()) > -1
-    );
-    if (auxDataFirst.length < 1) return auxDataFirst;
-    let auxDataLast = auxDataFirst.filter(
-      (user) => user.last.toLowerCase().indexOf(last.toLowerCase()) > -1
-    );
-    if (auxDataLast.length < 1) return auxDataLast;
-    let auxDataId = auxDataLast.filter(
-      (user) => user.id.toLowerCase().indexOf(id.toLowerCase()) > -1
-    );
-    if (auxDataId.length < 1) return auxDataId;
-    let auxDataRol = auxDataId.filter((user) => user.rol === rol);
-    if (auxDataRol.length < 1) return auxDataRol;
-    let auxDataState = auxDataRol.filter((user) => user.state === state);
-    if (auxDataState.length < 1) return auxDataState;
-    let auxDataPhone = auxDataState.filter(
-      (user) => user.phone.toLowerCase().indexOf(phone.toLowerCase()) > -1
-    );
-    if (auxDataPhone.length < 1) return auxDataPhone;
-    let auxDataEmail = auxDataPhone.filter(
-      (user) => user.email.toLowerCase().indexOf(email.toLowerCase()) > -1
-    );
-
-    return auxDataEmail;
-  };
+  const _filterData = (data, filters) => {};
 
   useEffect(() => {
     let auxData = [...data];
     if (filterData) {
-      const { first, last, id, rol, state, phone, email } = filterData;
-      console.log(filterData.first);
+      for (var key in filterData) {
+        if (filterData[key] !== "") {
+          let aux = auxData.filter((user) => {
+            user.first = user.name.first;
+            user.last = user.name.last;
+            if (key === "state")
+              return user[key].toLowerCase() === filterData[key].toLowerCase();
 
-      /* 
-      setDataSource(
-         auxData.filter((user) => {
-          user.first = user.name.first;
-          user.last = user.name.last;
-          console.log(user);
-          console.log(user.state);
-          console.log(state);
-          console.log(user.first.toLowerCase().indexOf(first.toLowerCase())); 
-
-           if (
-                    first !== "" && 
-              user.first.toLowerCase().indexOf(first.toLowerCase()) > -1 &&
-                  state !== "" && 
-              user.state.toLowerCase().indexOf(state.toLowerCase()) > -1
-            ) {
-              console.log("retorna true");
-              return true;
-            }
-            console.log("retorna falso");
-            return false; 
-
-                   console.log(user.rol.toLowerCase() === rol.toLowerCase());
-          console.log(
-            user.name.first?.toLowerCase().indexOf(name?.toLowerCase()) > -1
-          ); 
-
-           if (
-            user.name.first?.toLowerCase().indexOf(name?.toLowerCase()) > -1 ||
-            user.name.last?.toLowerCase().indexOf(last?.toLowerCase()) > -1 ||
-            user.id.value?.toLowerCase().indexOf(id?.toLowerCase()) > -1 ||
-                  user.rol.toLowerCase() === rol.toLowerCase() &&
-            user.state.toLowerCase() === state.toLowerCase() && 
-            user.phone?.toLowerCase().indexOf(phone?.toLowerCase()) > -1 ||
-            user.email?.toLowerCase().indexOf(email?.toLowerCase()) > -1
-          ) {
-            console.log("retorna true");
-            return true;
-          } 
-        })
-      ); */
+            return user[key]
+              .toLowerCase()
+              .includes(filterData[key].toLowerCase());
+          });
+          auxData = aux;
+        }
+      }
+      setDataSource(auxData);
     } else {
       setDataSource(data);
     }
@@ -172,12 +117,10 @@ const Roles = () => {
   };
 
   const onSubmitForm = (newUser) => {
-    console.log(newUser);
     setData([...data, newUser]);
     setDataSource([...data, newUser]);
 
     setShowModal(false);
-    console.log("submit");
   };
 
   return (
